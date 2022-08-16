5b82e48c53bb74423a2ff13698f86e94150c5da6
Michelle Shirk <michelle.l.shirk@gmail.com>
2022-08-16 19:02:02 -0400

    build file structure

---
+++ app.py
@@ -0,0 +1,175 @@
+import os
+
+from flask import Flask, render_template, request, flash, redirect, session, g
+from flask_debugtoolbar import DebugToolbarExtension
+from sqlalchemy.exc import IntegrityError
+
+from forms import UserAddForm, UserEditForm, LoginForm
+from models import db, connect_db, User
+
+CURR_USER_KEY = "curr_user"
+
+app = Flask(__name__)
+
+# Get DB_URI from environ variable (useful for production/testing) or,
+# if not set there, use development local db.
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgresql:///restroom-finder'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+
+
+##############################################################################
+# User signup/login/logout
+
+
+# @app.before_request
+# def add_user_to_g():
+#     """If we're logged in, add curr user to Flask global."""
+
+#     if CURR_USER_KEY in session:
+#         g.user = User.query.get(session[CURR_USER_KEY])
+
+#     else:
+#         g.user = None
+
+
+# def do_login(user):
+#     """Log in user."""
+
+#     session[CURR_USER_KEY] = user.id
+
+
+# def do_logout():
+#     """Logout user."""
+
+#     if CURR_USER_KEY in session:
+#         del session[CURR_USER_KEY]
+
+
+# @app.route('/signup', methods=["GET", "POST"])
+# def signup():
+#     """Handle user signup.
+
+#     Create new user and add to DB. Redirect to home page.
+
+#     If form not valid, present form.
+
+#     If the there already is a user with that username: flash message
+#     and re-present form.
+#     """
+
+#     form = UserAddForm()
+
+#     if form.validate_on_submit():
+#         try:
+#             user = User.signup(
+#                 username=form.username.data,
+#                 password=form.password.data,
+#                 email=form.email.data,
+#                 image_url=form.image_url.data or User.image_url.default.arg,
+#             )
+#             db.session.commit()
+
+#         except IntegrityError:
+#             flash("Username already taken", 'danger')
+#             return render_template('users/signup.html', form=form)
+
+#         do_login(user)
+
+#         return redirect("/")
+
+#     else:
+#         return render_template('users/signup.html', form=form)
+
+
+# @app.route('/login', methods=["GET", "POST"])
+# def login():
+#     """Handle user login."""
+
+#     form = LoginForm()
+
+#     if form.validate_on_submit():
+#         user = User.authenticate(form.username.data,
+#                                  form.password.data)
+
+#         if user:
+#             do_login(user)
+#             flash(f"Hello, {user.username}!", "success")
+#             return redirect("/")
+
+#         flash("Invalid credentials.", 'danger')
+
+#     return render_template('users/login.html', form=form)
+
+
+# @app.route('/logout')
+# def logout():
+#     """Handle logout of user."""
+
+#     do_logout()
+#     flash("Goodbye!", "success")
+#     return redirect("/login")
+
+
+
+
+
+# ##############################################################################
+# # Homepage and error pages
+
+
+# @app.route('/')
+# def homepage():
+#     """Show homepage:
+#     - anon users: no messages
+#     - logged in: 100 most recent messages of followed_users
+#     """
+
+#     if g.user:
+#         following_ids = [f.id for f in g.user.following] + [g.user.id]
+
+#         messages = (Message
+#                     .query
+#                     .filter(Message.user_id.in_(following_ids))
+#                     .order_by(Message.timestamp.desc())
+#                     .limit(100)
+#                     .all())
+
+#         liked_msg_ids = [msg.id for msg in g.user.likes]
+
+#         return render_template('home.html', messages=messages, likes=liked_msg_ids)
+
+#     else:
+#         return render_template('home-anon.html')
+
+
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     """404 Page Not Found"""
+
+#     return render_template('404.html'), 404
+
+
+# ##############################################################################
+# # Turn off all caching in Flask
+# #   (useful for dev; in production, this kind of stuff is typically
+# #   handled elsewhere)
+# #
+# # https://stackoverflow.com/questions/34066804/disabling-caching-in-flask
+
+# @app.after_request
+# def add_header(req):
+#     """Add non-caching headers on every request."""
+
+#     req.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#     req.headers["Pragma"] = "no-cache"
+#     req.headers["Expires"] = "0"
+#     req.headers['Cache-Control'] = 'public, max-age=0'
+#     return req
