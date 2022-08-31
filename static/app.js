01b330242763935ee7e9676386c5497dfc0d61dd
Michelle Shirk <michelle.l.shirk@gmail.com>
2022-08-31 18:29:22 -0400

    implement map

--- static/app.js
+++ static/app.js
@@ -7,134 +7,76 @@ let USE_LOCATION;
 let GEOCODER;
 const $geocoderDiv = $('#geocoder');
 const $searchButton = $('#search-button');
-const $useLocation = $('#use-location');
 
 // Search filters
 const $isAccessible = $('#accessible');
 const $isUnisex = $('#unisex');
 const $hasChangingTable = $('#changing-table');
 
-// Search output
+// Search results
 let RESTROOM_RESULTS = new Map();
 let CURRENT_MARKERS = [];
-const NUM_RESULTS = 100;
+let MAP;
+const NUM_RESULTS = 10;
 const $resultsContainer = $('#results-container');
 const $listContainer = $('#list-container');
 const $resultsList = $('#results-list');
 const $mapContainer = $('#map-container');
+const $map = $('#map');
 
 
-//////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// Search Event Handler
 
-// Search query Event Handler
 $searchButton.on('click', (evt) => {
     evt.preventDefault();
-    handleSearchByQuery();
-})
+    handleSearch();
+});
 
-// Search Current Location Event Handler
-$useLocation.on('click', (evt) => {
-    evt.preventDefault();
-    handleSearchByLocation();
-})
 
-const handleSearchByQuery = async () => {
+const handleSearch = async () => {
 
-    // clear previous search results from internal storage, results list, and map
-    RESTROOM_RESULTS.clear();
+    showSpinner();
+
+    // clear previous search results from results list, internal storage and map
     $resultsList.empty();
+    RESTROOM_RESULTS.clear();
     clearMapMarkers();
 
-    // get locations based on query
-    const $query = $('#query').val()
-    console.log($query);
+    // get coordinates based on current or default location
+    const coords = getCoordinates();
 
-    await getResultsByQuery($query);
-}
+    adjustMapForDeviceSize(coords.lat, coords.lon);
 
-const getResultsByQuery = async ($query) => {
+    await getResults(coords.lat, coords.lon);
 
-    const per_page = NUM_RESULTS;
+    hideSpinner();
 
-    resp = axios
-        .get(`${BASE_URL}/v1/restrooms/search?page=1&per_page=${per_page}&offset=0&query=${$query}&ada=${$isAccessible.is(':checked')}&unisex=${$isUnisex.is(':checked')}`)
-        
-        .then(async (resp) => {
-            let restrooms = resp.data;
-    
-            if (!restrooms){
-                console.log('no restrooms found')
-                return}
-            for (let i = 0; i < restrooms.length; i++){
-                const restroom = restrooms[i];
-        
-                const restroomData = {
-                    name: restroom.name,
-                    lat: restroom.latitude,
-                    lon: restroom.longitude,
-                };
-                
-                console.log(restroomData);
-                restroom.number = RESTROOM_RESULTS.size + 1;
-                RESTROOM_RESULTS.set(restroom.id, restroom);
-        
-                addMapMarker(restroom);
-                addResultToDOM(restroom);
-        
-                if (RESTROOM_RESULTS.size === NUM_RESULTS){
-                    break;
-                }
-            }
-        
-            return restrooms
-        })
-        .catch((err) => {
-            console.log(err);
-        });
 };
 
 
-const handleSearchByLocation = async () => {
-
-    // clear previous search results from internal storage, results list, and map
-    RESTROOM_RESULTS.clear();
-    $resultsList.empty();
-    clearMapMarkers();
-
-    // get coordinates based on current location
-    await setCurrentLocation();
-
-}
-
-const setCurrentLocation = async () => {
-    // Default coordinates
-    CURRENT_LAT = 39.9543;
-    CURRENT_LON = 75.1657;
-    USE_LOCATION = false;
-
-    if(!navigator.geolocation) {
-        console.log('Geolocation is not supported')
-        await showMap(CURRENT_LAT, CURRENT_LON);
-    } else {
-        navigator.geolocation.getCurrentPosition(
-            async (position) => {
-                CURRENT_LAT = position.coords.latitude;
-                CURRENT_LON = position.coords.longitude;
-                USE_LOCATION = true;
-                console.log('lat, lon = ', CURRENT_LAT, CURRENT_LON)
-                await showMap(CURRENT_LAT, CURRENT_LON);
-                await getResultsByLocation(CURRENT_LAT, CURRENT_LON);
-                return coords = {
-                    lat: CURRENT_LAT,
-                    lon: CURRENT_LON,
-                };
-            }
-        );
+const getCoordinates = () => {
+    return coordinates = {
+        lat: CURRENT_LAT,
+        lon: CURRENT_LON,
     }
 };
 
 
-const getResultsByLocation = async (lat, lon) => {
+const adjustMapForDeviceSize = (lat, lon) => {
+// ajusts display so map and list display side by side on larger screens
+    if ($(window).width() > 750) {
+        if (!($mapContainer).hasClass('map-large')){
+            $mapContainer.addClass('col-md-8 map-large');
+            $map.addClass('map-large');
+        }
+
+        MAP.center = [lon, lat];
+    }
+};
+
+
+const getResults = async (lat, lon) => {
 
     const per_page = NUM_RESULTS;
 
@@ -179,10 +121,105 @@ const getResultsByLocation = async (lat, lon) => {
         });
 };
 
+/////////////////////////////////////////////////
+// Map Functions
+
+const showMap = async(lat, lon) => {
+    $geocoderDiv.empty();
+
+    MAP = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      zoom: 13,
+      center: [lon, lat],
+    });
+  
+    let nav = new mapboxgl.NavigationControl();
+    MAP.addControl(nav, 'top-right');
+  
+    GEOCODER = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl,
+      zoom: 13,
+    });
+  
+    document.getElementById('geocoder').appendChild(GEOCODER.onAdd(MAP));
+  
+    if (USE_LOCATION) {
+      // search using reverse geocode query string
+      const resp = await axios.post('/api/reverse-geocode', { lat, lon });
+      const query_string = resp.data.result;
+      GEOCODER.query(query_string);
+    }
+
+};
+
+
+const refreshMap = (lat, lon) => {
+    $geocoderDiv.empty();
+    if (GEOCODER.mapMarker){
+        GEOCODER.mapMarker.remove();
+    }
+
+    MAP.center = [lon, lat];
+
+    GEOCODER = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+        zoom: 13,
+    });
+
+    document.getElementById('geocoder').appendChild(GEOCODER.onAdd(MAP));
+
+    adjustMapForDeviceSize(lat, lon);
+};
+
 
 const addMapMarker = (restroom) => {
+    const {
+        number,
+        id,
+        name,
+        longitude,
+        latitude,
+        street,
+        city,
+        state,
+        distance,
+        details,
+        accessible,
+        unisex,
+        changing_table,
+      } = restroom;
 
-}
+    const marker = {
+        type: 'Feature',
+        properties: {
+            message: name,
+        },
+        geometry: {
+            type:'Point',
+            coordinates: [longitude, latitude],
+        },
+    };
+    
+    const el = document.createElement('div');
+    el.className = 'marker';
+    el.innerText = number;
+
+    const newMarker = new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .setPopup(
+            new mapboxgl.Popup().setHTML(`
+                <p class="popup-title">${name}<span class="float-right font-weight-normal ml-5"><i>   ${distance.toFixed(2)} mi</i></span></p>
+                <p class="popup-address">${street}</p>
+                <p class="popup-address">${city}, ${state}</p>
+            `)
+        )
+        .addTo(MAP);
+    CURRENT_MARKERS.push(newMarker);
+};
+
 
 const clearMapMarkers = () => {
     if (CURRENT_MARKERS) {
@@ -190,7 +227,10 @@ const clearMapMarkers = () => {
             CURRENT_MARKERS[i].remove();
         }
     }
-}
+};
+
+/////////////////////////////////////////////////
+// Results List
 
 const addResultToDOM = (restroom) => {
     const {
@@ -237,43 +277,55 @@ const addResultToDOM = (restroom) => {
     $resultsList.append(newLi);
 };
 
-const showMap = async(lat, lon) => {
-    $geocoderDiv.empty();
+/////////////////////////////////////////////////
+// Loading Spinner
 
-    MAP = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      zoom: 11,
-      center: [lon, lat],
-    });
-  
-    let nav = new mapboxgl.NavigationControl();
-    MAP.addControl(nav, 'top-right');
-  
-    GEOCODER = new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl,
-      zoom: 11,
-    });
-  
-    document.getElementById('geocoder').appendChild(GEOCODER.onAdd(MAP));
-  
-    if (USE_LOCATION) {
-      // get reverse gecode search string to input into search box
-      const resp = await axios.post('/api/reverse-geocode', { lat, lon });
-      const query_string = resp.data.result;
-      GEOCODER.query(query_string);
-    }
-  
-    const layerList = document.getElementById('menu');
-    const inputs = layerList.getElementsByTagName('input');
-  
-    function switchLayer(layer) {
-      const layerId = layer.target.id;
-      MAP.setStyle('mapbox://styles/mapbox/' + layerId);
-    }
-  
-    for (var i = 0; i < inputs.length; i++) {
-      inputs[i].onclick = switchLayer;
-    }
+const showSpinner = () => {
+// search button becomes loading spinner
+    ($searchButton).attr("data-original-text", $($searchButton).html());
+    ($searchButton).prop("disabled", true);
+    ($searchButton).html('<i class="spinner-border spinner-border-sm"></i> Loading...');
 };
+
+
+const hideSpinner = () => {
+    ($searchButton).prop("disabled", false);
+    ($searchButton).html(($searchButton).attr("data-original-text"));
+};
+
+
+/////////////////////////////////////////////////
+// Initialize and get current location
+
+const setCurrentLocation = async () => {
+    // Default coordinates
+    CURRENT_LAT = 39.954328;
+    CURRENT_LON = -75.165717;
+    USE_LOCATION = false;
+
+    if(!navigator.geolocation) {
+        console.log('Geolocation is not supported')
+        await showMap(CURRENT_LAT, CURRENT_LON);
+    } else {
+        navigator.geolocation.getCurrentPosition(
+            async (position) => {
+                CURRENT_LAT = position.coords.latitude;
+                CURRENT_LON = position.coords.longitude;
+                USE_LOCATION = true;
+                console.log('lat, lon = ', CURRENT_LAT, CURRENT_LON)
+                await showMap(CURRENT_LAT, CURRENT_LON);
+            },
+            async () => {
+                await showMap(CURRENT_LAT, CURRENT_LON);
+            }
+        );
+    };
+};
+
+
+const initialize = () => {
+    setCurrentLocation();
+};
+
+
+$(initialize());
