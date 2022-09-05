81e87d956af0991643d20907dac92d5fbcabe5f1
Michelle Shirk <michelle.l.shirk@gmail.com>
2022-09-05 18:47:41 -0400

    heroku deployment

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 app = Flask(__name__)
 
 
-uri = os.environ.get('DATABASE_URL', 'postgresql:///mshirk-flusher')
+uri = os.environ.get('DATABASE_URL')
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = (
