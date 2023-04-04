418ba9d8586bdcc29a6e01df46c3cbc39dc25f47
Michelle Shirk <michelle.l.shirk@gmail.com>
2023-04-04 14:29:35 -0400

    update DATABASE_URL to comply with Heroku

--- app.py
+++ app.py
@@ -7,10 +7,14 @@ import os
 
 app = Flask(__name__)
 
+# for running locally
+# uri = 'postgresql:///flusher'
 
-uri = os.environ.get('DATABASE_URL') or 'postgresql:///flusher'
+# for running on Heroku
+uri = os.environ.get('DATABASE_URL')
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
